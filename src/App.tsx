0b0146a455487f64f9a892d171de448c6f7ba09b
Mark Levison <mark@mlevison.com>
2020-10-13 11:45:21 -0400

    FirstImplemenation of BuildServer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
 import React, { useReducer } from 'react';
 import { storySucceeds } from './lib';
 
+type GameAction = {
+  effect: number;
+};
+
 type RoundState = {
+  selectedGameActions: GameAction[];
   round: number;
+  capacity: number;
   storiesCompleted: number;
 };
-const INITIAL_STATE: RoundState = { round: 1, storiesCompleted: 0 };
+const INITIAL_STATE: RoundState = {
+  selectedGameActions: [],
+  round: 1,
+  storiesCompleted: 0,
+  capacity: 10
+};
 const RESULTS_ROUND = 7;
 
+type NextRoundAction = { type: 'NEXT_ROUND' };
+type AddGameActionAction = { type: 'ADD_GAME_ACTION', payload: GameAction };
+type Action = NextRoundAction | AddGameActionAction;
+
+function sumUpEffects(gameActions: GameAction[]): number {
+  let totalEffects = 0;
+
+  gameActions.forEach(({ effect }) => {
+    totalEffects += effect;
+  });
+
+  return totalEffects;
+}
+
 export default function App() {
-  const [{ round, storiesCompleted }, nextRound] = useReducer(
-    (state: RoundState) => {
-      return {
-        round: state.round + 1,
-        storiesCompleted:
-          state.storiesCompleted +
-          Array(10).fill('').filter(storySucceeds).length,
-      };
+  const [state, dispatch] = useReducer(
+    (state: RoundState, action: Action) => {
+      switch (action.type) {
+        case "ADD_GAME_ACTION":
+          return {
+            ...state,
+            selectedGameActions: [
+              ...state.selectedGameActions,
+              action.payload
+            ]
+          };
+        case 'NEXT_ROUND':
+          return {
+            ...state,
+            capacity: state.capacity + sumUpEffects(state.selectedGameActions),
+            selectedGameActions: [],
+            round: state.round + 1,
+            storiesCompleted:
+              state.storiesCompleted +
+              Array(10).fill('').filter(storySucceeds).length,
+          };
+      }
     },
     INITIAL_STATE,
   );
+  const { round, storiesCompleted, capacity } = state;
 
   return (
     <>
@@ -31,10 +71,18 @@ export default function App() {
         </>
       ) : (
         <>
+          <button onClick={() => dispatch({
+            type: 'ADD_GAME_ACTION',
+            payload: {
+              effect: 1
+            }
+          })}>BuildServer</button>
+          <p>Capacity: {capacity}</p>
           <h2>Round {round} of 6</h2>
-          <button onClick={nextRound}>Complete Round</button>
+          <button onClick={() => dispatch({ type: 'NEXT_ROUND' })}>Complete Round</button>
         </>
       )}
     </>
   );
 }
+
