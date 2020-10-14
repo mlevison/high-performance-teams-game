238db09b8ad3bc3901bd5f7973176a002bbd9168
Xiphe <github@xiphe.net>
2020-10-14 20:53:35 +0200

    refactor(App): access state properties directly
    instead of using destructuring
    
    in order make codebase more easy to understand

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,19 @@ const RESULTS_ROUND = 7;
 
 export default function App() {
   const [state, dispatch] = useReducer(gameReducer, INITIAL_STATE);
-  // I don't understand this declaration its like magic.
-  const { round, storiesCompleted, capacity, actionCostThisRound } = state;
 
   return (
     <>
       <h1>High-Performance Team Game </h1>
-      {round === RESULTS_ROUND ? (
+      {state.round === RESULTS_ROUND ? (
         <>
           <h2>Results</h2>
-          <p>Completed {storiesCompleted} user stories</p>
+          <p>Completed {state.storiesCompleted} user stories</p>
         </>
       ) : (
         <>
-          <p>Round: {round - 1} results</p>
-          <p>Action Cost: {actionCostThisRound} </p>
+          <p>Round: {state.round - 1} results</p>
+          <p>Action Cost: {state.actionCostThisRound} </p>
           <button
             onClick={() =>
               dispatch({
@@ -33,8 +31,8 @@ export default function App() {
           >
             BuildServer Cost
           </button>
-          <p>Capacity: {capacity}</p>
-          <h2>Round {round} of 6</h2>
+          <p>Capacity: {state.capacity}</p>
+          <h2>Round {state.round} of 6</h2>
           <button onClick={() => dispatch({ type: 'NEXT_ROUND' })}>
             Complete Round
           </button>
