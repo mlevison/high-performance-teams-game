8d6ce61e044437f9e4fecb9d547a8e7df5fb99e3
Mark Levison <mark@mlevison.com>
2020-10-16 16:17:16 -0400

    Game state machine basic OO version
    A basic OO implementation of the game state machine.

--- src/gameState/gameSamplePlays.spec.ts
+++ src/gameState/gameSamplePlays.spec.ts
@@ -2,138 +2,249 @@
 import { GameAction } from './types';
 
 class CoreAction {
+  // TODO how to make const?
+  _createdRound: number;
+  constructor(createdRound: number) {
+    this._createdRound = createdRound;
+  }
+
   // TODO how do JavaScript programmers force methods to be overridden?
-  getCost() { return 0 };
-  getEffect() { return 0};
+  getCost() {
+    return 0;
+  }
+  getEffect(currentRound: number) {
+    return 0;
+  }
+
+  isEngineeringImprovement(): Boolean {
+    return false;
+  }
+
+  actionName(): String {
+    return '';
+  }
+
+  actionDescription(): String {
+    return '';
+  }
 }
 
-class BuilderServerAction extends CoreAction {
-  getCost() { return 2 };
+// TODO How to indicate this is intended to be a baseclass and never instatiated except for test
+class EngineeringAction extends CoreAction {
+  isEngineeringImprovement(): Boolean {
+    return true;
+  }
+}
+class BuilderServerAction extends EngineeringAction {
+  getCost() {
+    return 2;
+  }
+
+  actionName(): String {
+    return 'Build Server';
+  }
+
+  actionDescription(): String {
+    return 'Setup Build Server and Continuous Integration. This is required to make future engineering improvements';
+  }
 }
 
 class TeamMembersOnSameFloor extends CoreAction {
-  getCost() { return 3 };
+  getCost() {
+    return 3;
+  }
+  getEffect(currentRound: number) {
+    if (currentRound - this._createdRound < 5) {
+      return 1;
+    }
+    return 0;
+  }
+
+  actionName(): String {
+    return 'Team Members On SameFloor';
+  }
+
+  actionDescription(): String {
+    return "Getting Team Members on the same floor reduces the cost of communication as they don't have to go far to ask questions";
+  }
 }
 
-type Round = {
-  selectedGameActions: CoreAction[];
-  round: number;
-};
+class TechnicalDebtDrag extends CoreAction {
+  // TODO Find a way to tell this class it no longer has effect
+  _effect: number;
+  constructor() {
+    super(1);
+    this._effect = -1;
+  }
 
-const INTIAL_ROUND: Round = {
-  // TODO Array or Set
-  selectedGameActions: [],
-  round: 0
+  getEffect(currentRound: number) {
+    return this._effect;
+  }
+
+  disable() {
+    console.log('Technical Debt disabled');
+    this._effect = 0;
+  }
 }
 
-type Game = {
-  rounds: Round[];
-  currentRound : number;
-  currentCapacity : number;
+class Round {
+  _selectedGameActions: Set<CoreAction>;
+  _round: number;
+  constructor(round: number) {
+    this._round = round;
+    this._selectedGameActions = new Set<CoreAction>();
+  }
+
+  roundActionCost(): number {
+    let totalCosts = 0;
+
+    this._selectedGameActions.forEach(
+      (gameAction) => (totalCosts += gameAction.getCost()),
+    );
+
+    return totalCosts;
+  }
+
+  culmlativeEffectsForCurrentRound(currentRound: number): number {
+    let totalEffect = 0;
+
+    this._selectedGameActions.forEach(
+      (gameAction) => (totalEffect += gameAction.getEffect(currentRound)),
+    );
+
+    return totalEffect;
+  }
+
+  addCoreAction(coreAction: CoreAction) {
+    if (coreAction.isEngineeringImprovement()) {
+      this._selectedGameActions.forEach((existingAction) =>
+        existingAction instanceof TechnicalDebtDrag
+          ? existingAction.disable()
+          : null,
+      );
+    }
+    this._selectedGameActions.add(coreAction);
+  }
+}
+
+class Game {
+  _roundList: Round[];
+  _currentRound: number;
+  _currentCapacity: number;
   // Vastly less important than capacity
-  totalStoriesCompleted : number;
-}
+  _totalStoriesCompleted: number;
 
-const INITIAL_GAME: Game = {
-  rounds: [],
-  currentRound: 1,
-  currentCapacity: 10,
-  totalStoriesCompleted: 0,
-};
+  constructor() {
+    // TODO ugghh plural to indicate array
+    this._roundList = [new Round(1)];
+    this._roundList[0].addCoreAction(new TechnicalDebtDrag());
+    this._currentRound = 1;
+    this._currentCapacity = 10;
+    this._totalStoriesCompleted = 0;
+  }
 
-function completeSprint(game : Game): Game {
-  let newCapacity = game.currentCapacity - 1;
-  let nextRound = game.currentRound + 1
-  return { ...game, currentCapacity: newCapacity, currentRound: nextRound };
-}
+  completeSprint() {
+    let capacityChange = 0;
+    for (const round of this._roundList) {
+      capacityChange += round.culmlativeEffectsForCurrentRound(
+        this._currentRound,
+      );
+    }
 
-// TODO Object Oriented Mark would normally make round a class to encapsulate the math and the data
-function roundActionCost(round: Round): number {
-  let totalCosts = 0;
+    this._currentCapacity += capacityChange;
+    this._currentRound++;
+  }
 
-  for (const gameAction of round.selectedGameActions) {
-    totalCosts += gameAction.getCost();
-  };
+  addCoreAction(coreAction: CoreAction) {
+    // -1 as arrays are 0 based and Sprints start 1
+    this._roundList[this._currentRound - 1].addCoreAction(coreAction);
+  }
 
-  return totalCosts;
+  currentRoundCost(): number {
+    // -1 as arrays are 0 based and Sprints start 1
+    return this._roundList[this._currentRound - 1].roundActionCost();
+  }
+
+  getCurrentRound(): number {
+    return this._currentRound;
+  }
+
+  getCurrentCapacity(): number {
+    return this._currentCapacity;
+  }
 }
 
 describe('GameSamplePlays', () => {
   describe('Demonstrate Drag Effect if the team make no engineering improvement', () => {
     it('no Enginerring Improvement', () => {
       /* TDOD ideally if understood the functional style these would const or perhaps chained */
-      let gameExample = INITIAL_GAME;
+      let gameExample = new Game();
       // Ideally Stated as a precondition
-      expect(gameExample.currentRound).toEqual(1);
+      expect(gameExample.getCurrentRound()).toEqual(1);
 
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentRound).toEqual(2);
-      expect(gameExample.currentCapacity).toEqual(9);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentRound()).toEqual(2);
+      expect(gameExample.getCurrentCapacity()).toEqual(9);
 
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentRound).toEqual(3);
-      expect(gameExample.currentCapacity).toEqual(8)
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentRound()).toEqual(3);
+      expect(gameExample.getCurrentCapacity()).toEqual(8);
 
-     gameExample = completeSprint(gameExample);
-     expect(gameExample.currentRound).toEqual(4);
-     expect(gameExample.currentCapacity).toEqual(7);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentRound()).toEqual(4);
+      expect(gameExample.getCurrentCapacity()).toEqual(7);
     });
 
     it('Adding a BuildServer eliminates Drag Effect and has no positive effect on capacity', () => {
-      let oneRoundOneAction = INTIAL_ROUND;
+      let gameExample = new Game();
       // TODO Really this should be typed as buildServer and then we could detect that in completeSprint and
-      oneRoundOneAction.selectedGameActions[0] = new BuilderServerAction();
-      let gameExample = INITIAL_GAME;
-      gameExample.rounds[0] =  oneRoundOneAction;
+      gameExample.addCoreAction(new BuilderServerAction(1));
 
-      expect(roundActionCost(gameExample.rounds[0])).toEqual(2);
+      expect(gameExample.currentRoundCost()).toEqual(2);
 
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentRound).toEqual(2);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentRound()).toEqual(2);
       // Capacity unchanged because we eliminate the drag effect
-      expect(gameExample.currentCapacity).toEqual(10);
+      expect(gameExample.getCurrentCapacity()).toEqual(10);
 
-      gameExample = completeSprint(completeSprint(gameExample));
+      gameExample.completeSprint();
+      gameExample.completeSprint();
 
-      expect(gameExample.currentRound).toEqual(4);
+      expect(gameExample.getCurrentRound()).toEqual(4);
       // Capacity still **unchanged**
-      expect(gameExample.currentCapacity).toEqual(10);
+      expect(gameExample.getCurrentCapacity()).toEqual(10);
     });
   });
 
   describe('Team members on the same floor improve communication', () => {
     it('Prove that the action takes effect over many rounds', () => {
-      let oneRoundOneAction = INTIAL_ROUND;
+      let gameExample = new Game();
       // TODO Really this should be typed as TeamMembersOnSameFloor and then we could detect that in completeSprint and
-      oneRoundOneAction.selectedGameActions[0] = new TeamMembersOnSameFloor();
-      let gameExample = INITIAL_GAME;
-      gameExample.rounds[0] =  oneRoundOneAction;
+      gameExample.addCoreAction(new EngineeringAction(1));
+      gameExample.addCoreAction(new TeamMembersOnSameFloor(1));
 
-      expect(roundActionCost(gameExample.rounds[0])).toEqual(3);
+      expect(gameExample.currentRoundCost()).toEqual(3);
 
       // Effect we're looking for: 1 Capacity, per round, for 5 rounds, accounting for face to face time
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentRound).toEqual(2);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentRound()).toEqual(2);
       // Capacity improved by one each round
-      expect(gameExample.currentCapacity).toEqual(11);
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentCapacity).toEqual(12);
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentCapacity).toEqual(13);
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentCapacity).toEqual(14);
-      gameExample = completeSprint(gameExample);
-      expect(gameExample.currentCapacity).toEqual(15);
+      expect(gameExample.getCurrentCapacity()).toEqual(11);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentCapacity()).toEqual(12);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentCapacity()).toEqual(13);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentCapacity()).toEqual(14);
+      gameExample.completeSprint();
+      expect(gameExample.getCurrentCapacity()).toEqual(15);
     });
   });
 
   describe('Round 2 Actions', () => {
-    it('Social Time only Avialble in Round 2', ()=>{
+    it('Social Time only Avialble in Round 2', () => {
       // Should we test for this explicitly or simply enforce by convention --- only make
-
     });
-    it('Unit Testing Only Avialble if the BuildServer was implemented', ()=>{
-
-    });
+    it('Unit Testing Only Avialble if the BuildServer was implemented', () => {});
   });
 });
