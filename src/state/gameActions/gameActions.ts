a0de6c35e36bbf0892da0794cf4b2b78e479eb13
Mark Levison <mark@mlevison.com>
2020-10-27 16:15:08 -0400

    Add a few actions and a note with design questions

--- src/state/gameActions/gameActions.ts
+++ src/state/gameActions/gameActions.ts
@@ -5,9 +5,36 @@ export type GameActionId =
   | 'GAME_ACTION_BUILD_SERVER'
   | 'GAME_ACTION_TEAMS_ON_SAME_FLOOR'
   | 'GAME_ACTION_UNIT_TESTING'
-  | 'GAME_ACTION_INFORMAL_CROSS_TRAING';
+  | 'GAME_ACTION_INFORMAL_CROSS_TRAING'
+  | 'PROTECTED_FROM_OUTSIDE_DISTRACTION'
+  | 'WORKING_AGREEMENTS'
+  | 'ELIMINATE_LONG_LIVED_FEATURE_BRANCHES';
 
 export const gameActionList: GameActionList = {
+  PROTECTED_FROM_OUTSIDE_DISTRACTION: {
+    name: 'Protected from Outside Distraction',
+    available: unique(),
+    description: 'ScrumMaster protects the team from outside distraction',
+    cost: 1,
+    // TODO - the effect is different in that it effects the success die roll
+    effect: hasNoEffect,
+  },
+  WORKING_AGREEMENTS: {
+    name: 'Working Agreements',
+    available: unique(),
+    description: 'Create Team Working Agreements',
+    cost: 1,
+    // TODO - the effect is different in that it effects the success die roll
+    effect: () => ({ capacity: 1 }),
+  },
+  ELIMINATE_LONG_LIVED_FEATURE_BRANCHES: {
+    name: 'All Work is done on Main or Trunk',
+    available: unique(),
+    description:
+      'When teams use Feature Branches – then they’re not really using Continuous integration. Feature branching optimizes for the individual while harming the Team',
+    cost: 2,
+    effect: () => ({ capacity: 1 }),
+  },
   GAME_ACTION_BUILD_SERVER: {
     name: 'Build Server',
     available: unique(),
