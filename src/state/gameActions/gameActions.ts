02ecf87f4133105747951e910bbd5ef1d9c601bf
Mark Levison <mark@mlevison.com>
2020-10-27 12:49:16 -0400

    Update gameActions.ts
    One new action and failed improvement

--- src/state/gameActions/gameActions.ts
+++ src/state/gameActions/gameActions.ts
@@ -4,7 +4,8 @@ import { unique, hasNoEffect, combine, fromRound, requires } from './helpers';
 export type GameActionId =
   | 'GAME_ACTION_BUILD_SERVER'
   | 'GAME_ACTION_TEAMS_ON_SAME_FLOOR'
-  | 'GAME_ACTION_UNIT_TESTING';
+  | 'GAME_ACTION_UNIT_TESTING'
+  | 'GAME_ACTION_INFORMAL_CROSS_TRAING';
 
 export const gameActionList: GameActionList = {
   GAME_ACTION_BUILD_SERVER: {
@@ -21,7 +22,7 @@ export const gameActionList: GameActionList = {
     description:
       "Getting Team Members on the same floor reduces the cost of communication as they don't have to go far to ask questions",
     cost: 3,
-    effect: () => ({ capacity: 2 }),
+    effect: (age) => ({ capacity: age < 5 ? 1 : 5 }),
   },
   GAME_ACTION_UNIT_TESTING: {
     name: 'Unit Testing',
@@ -33,5 +34,13 @@ export const gameActionList: GameActionList = {
     description: 'TODO: SOME DESCRIPTION',
     cost: 2,
     effect: () => ({ capacity: 2 }),
+  },
+  GAME_ACTION_INFORMAL_CROSS_TRAING: {
+    name: 'Informal Cross Training',
+    available: combine([unique(), fromRound(3)]),
+    description:
+      'Informal cross-training for existing team members in an area the team is weak. (Testing anyone?)',
+    cost: 1,
+    effect: () => ({ capacity: 1 }),
   },
 };
