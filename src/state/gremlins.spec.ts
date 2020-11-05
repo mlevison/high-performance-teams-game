8b81394c808d87688cb3372642a78e5f0a18682b
Xiphe <github@xiphe.net>
2020-11-05 18:17:19 +0100

    test(gremlins): add spec for "emergency on another team"

--- src/state/gremlins.spec.ts
+++ src/state/gremlins.spec.ts
@@ -1,16 +1,87 @@
-import { renderHook, act, HookResult } from '@testing-library/react-hooks';
-import { GameActionId } from './gameActions/gameActions';
-import { GremlinId } from './gremlins';
-import useAppState, { AppState } from './useAppState';
+import { getGame } from '../lib/testHelpers';
 
 /* disable game effect to only tests gremlin effects */
+jest.mock('./roundDescriptions/roundDescriptions', () => ({
+  roundDescriptions: {
+    1: {
+      description: null,
+      effect: () => ({ capacity: 10 }),
+    },
+  },
+}));
 jest.mock('./effects/effects', () => ({
   gameEffectList: [],
 }));
 
 describe('Gremlins', () => {
-  // describe('emergency on another team', () => {
-  //   it('reduces capacity for 3 rounds', () => {
-  //   })
-  // })
+  describe('emergency on another team', () => {
+    it('reduces capacity by 3 for 3 rounds', () => {
+      const game = getGame();
+
+      game.nextRound('EMERGENCY_ON_ANOTHER_TEAM');
+      expect(game.state.currentRound.capacity.available).toBe(7);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(7);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(7);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+    });
+
+    it('reduces capacity by 3 for 2 rounds when team is protected by outside distractions', () => {
+      const game = getGame();
+
+      game.selectAction('PROTECTED_FROM_OUTSIDE_DISTRACTION');
+      game.nextRound('EMERGENCY_ON_ANOTHER_TEAM');
+      expect(game.state.currentRound.capacity.available).toBe(7);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(7);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+    });
+
+    it('reduces capacity by 2 for 3 rounds when team had informal cross training', () => {
+      const game = getGame();
+
+      game.selectAction('GAME_ACTION_INFORMAL_CROSS_TRAINING');
+      game.nextRound('EMERGENCY_ON_ANOTHER_TEAM');
+      expect(game.state.currentRound.capacity.available).toBe(8);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(8);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(8);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+    });
+
+    it('reduces capacity by 1 for 2 rounds when team has all protective actions', () => {
+      const game = getGame();
+
+      game.selectAction('GAME_ACTION_INFORMAL_CROSS_TRAINING');
+      game.selectAction('GAME_ACTION_FORMAL_CROSS_TRAINING');
+      game.selectAction('PROTECTED_FROM_OUTSIDE_DISTRACTION');
+      game.nextRound('EMERGENCY_ON_ANOTHER_TEAM');
+      expect(game.state.currentRound.capacity.available).toBe(9);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(9);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+
+      game.nextRound();
+      expect(game.state.currentRound.capacity.available).toBe(10);
+    });
+  });
 });
--- src/state/gremlins.ts
+++ src/state/gremlins.ts
@@ -1,18 +1,19 @@
 import { Effect } from './effects';
 
-export type GremlinId = 'TEAM_MEMBER_MANAGEMENT_RELATION';
+export type GremlinId = 'EMERGENCY_ON_ANOTHER_TEAM';
 type Gremlin = {
   title: string;
-  occursOn: number;
-  effect: () => null | Effect;
+  occurs: () => boolean;
+  effect: (age: number) => null | Effect;
 };
 type GremlinList = { [k in GremlinId]: Gremlin };
 
 export const gremlinList: GremlinList = {
-  TEAM_MEMBER_MANAGEMENT_RELATION: {
-    title: 'Team Member/Management Relationship',
-    occursOn: 2,
-    effect() {
+  EMERGENCY_ON_ANOTHER_TEAM: {
+    title:
+      'We’ve had an emergency on another team, we need your best tester for a while.',
+    occurs: () => Math.random() <= 0.1,
+    effect(age) {
       return null;
     },
   },
