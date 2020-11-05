71e6836ebc9732fb127e938b1831e089d86eec84
Xiphe <github@xiphe.net>
2020-11-05 17:04:45 +0100

    test(roundDescription3): test effect of round3

--- src/state/roundDescriptions/roundDescriptions.spec.tsx
+++ src/state/roundDescriptions/roundDescriptions.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook, HookResult } from '@testing-library/react-hooks';
+import { renderHook, HookResult, act } from '@testing-library/react-hooks';
 import useAppState, { AppState } from '../useAppState';
 import { render, screen } from '@testing-library/react';
 
@@ -9,6 +9,11 @@ jest.mock('../effects/effects', () => ({
 
 describe('roundDescriptions and effects', () => {
   let result: HookResult<ReturnType<typeof useAppState>>;
+  const nextRound = () => {
+    act(() => {
+      result.current[1]({ type: 'NEXT_ROUND' });
+    });
+  };
 
   describe('round 1', () => {
     it('starts with capacity 10/10 in round 1', () => {
@@ -30,6 +35,38 @@ describe('roundDescriptions and effects', () => {
       render(result.current[0].currentRound.description);
       expect(
         screen.getByText(/Welcome to the World’s Smallest Online Bookstore/i),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('round 3', () => {
+    it('comes with a 4 capacity bump', () => {
+      result = renderHook(() => useAppState()).result;
+
+      nextRound();
+      nextRound();
+      const expectedCurrentRound: AppState['currentRound'] = expect.objectContaining(
+        {
+          capacity: {
+            total: 14,
+            available: 14,
+          },
+          number: 3,
+          activeEffects: expect.any(Array),
+        },
+      );
+
+      expect(result.current[0].currentRound).toEqual(expectedCurrentRound);
+      expect(result.current[0].currentRound.activeEffects).toHaveLength(1);
+      const round3Effect = result.current[0].currentRound.activeEffects[0];
+      expect(round3Effect.capacity).toBe(4);
+      expect(round3Effect.title).toMatch(/Management is paying overtime/i);
+
+      render(result.current[0].currentRound.description);
+      expect(
+        screen.getByText(
+          /We must go live with an early version of the product/i,
+        ),
       ).toBeInTheDocument();
     });
   });
