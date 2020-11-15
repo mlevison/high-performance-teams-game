e1f08d3500e846e382a69ab7d7363b8d9dba25c0
Mark Levison <mark@mlevison.com>
2020-11-15 17:47:42 -0500

    First pass at rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -4,7 +4,19 @@ export default function Rules() {
   return (
     <>
       <h2>Rules</h2>
-      <p>...</p>
+      <p>
+        <ul>
+          <li>Rounds represent 6 weeks or 3 Sprints in the life of your team</li>
+          <li>The game lasts 10 rounds or 30 weeks</li>
+          <li>Your team starts with a Capacity of 10 pontsi</li>
+          <ul>
+            <li>This Capacity can be spent on User Stories</li>
+            <li>This Capacity can also be spent on Improvements (aka Actions)</li>
+          </ul>
+          <li>Teams that fail to improve both Communications and Engineering Practice will eventually be penalized.</li>
+        </ul>
+        Hint - play the game as often as you want. So if you want to run experiments to see what works - go for it.
+      </p>
     </>
   );
 }
