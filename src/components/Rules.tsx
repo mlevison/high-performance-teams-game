a637876058005a712ec3b8c9a990dcf635bbaad4
Mark Levison <mark@mlevison.com>
2020-11-17 12:25:15 -0500

    Small Improvements to rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -14,13 +14,15 @@ export default function Rules() {
             <li>This Capacity can also be spent on Improvements (aka Actions). These improvements cost the team Capacity this round and may provide benefit in future rounds.</li>
           </ul>
           <li>Teams that fail to improve both Communications and Engineering Practice will eventually be penalized.</li>
+          <li>If you play well your team's capacity will increase round by round</li>
         </ul>
         <h2>Rounds</h2>
         Each round consists of a series of steps:
         <ol>
           <li>You get a round description - Click <b>Start Round</b></li>
           <li>Select Improvements or Actions that will affect the team. Once selected the actions cost will be taken away from the teams Capacity Click <b>Complete Round</b></li>
-          <li>The game determines how many Stories you attempted and then roles a die to determine how many were completed. Click <b>Next Round</b></li>
+          <li>The Game tells you how much capacity there is to spend on User Stories.Click <b>Roll for User Stories</b></li>
+          <li>The game determines how many Stories were completed. Click <b>Next Round</b></li>
           <li>At the end of each round the game also checks to see if your team was affected by a Gremlin - example - "Manager yells at a team member"</li>
         </ol>
         Notice each round your Capacity increases or decreases depending on the improvements you have made and the gremlins that affect you.
