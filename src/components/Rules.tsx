596b90d4ebbd3c37ad5f730b29f1d0efdf39510f
Mark Levison <mark@mlevison.com>
2020-11-19 17:02:29 -0500

    Update rules to include a welcome page.

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 export default function Rules() {
   return (
     <>
+      <h2>Welcome</h2>
+      <p>Your team is working on the World’s Smallest Online Bookstore, a site that provides the best results (just a few) for every search, not every result on earth. We’re a vulture capital funded company, so if we don’t deliver, our funding will be cut.
+
+      My goal is to help you see the effects of choices/tradeoffs on productivity and team cohesion. While some of the benefits of Agile happen at the individual level, there are many things that affect the relationships between team members, and therefore the overall cohesion and productivity of the team.
+      </p>
       <h2>Rules</h2>
       <p>
         <ul>
@@ -19,10 +24,10 @@ export default function Rules() {
         <h2>Rounds</h2>
         Each round consists of a series of steps:
         <ol>
-          <li>You get a round description - Click <b>Start Round</b></li>
-          <li>Select Improvements or Actions that will affect the team. Once selected the actions cost will be taken away from the teams Capacity Click <b>Complete Round</b></li>
-          <li>The Game tells you how much capacity there is to spend on User Stories.Click <b>Roll for User Stories</b></li>
-          <li>The game determines how many Stories were completed. Click <b>Next Round</b></li>
+          <li>You get a round description. Click <b>Start Round</b></li>
+          <li>Select Improvements or Actions that will affect the team. Once selected the actions cost will be taken away from the teams Capacity Click. <b>Complete Round</b></li>
+          <li>The Game tells you how much capacity there is to spend on User Stories. Click <b>Roll for User Stories</b> </li>
+          <li>The rolls a die for each User Story to determine how many are actually completed. Click <b>Next Round</b></li>
           <li>At the end of each round the game also checks to see if your team was affected by a Gremlin - example - "Manager yells at a team member"</li>
         </ol>
         Notice each round your Capacity increases or decreases depending on the improvements you have made and the gremlins that affect you.
