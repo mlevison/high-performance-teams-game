9036b2d03b95ba223c6351992fad45f6a9a0f44a
Xiphe <github@xiphe.net>
2021-04-02 21:01:16 +0200

    fix: add empty 8th round
    ref https://github.com/mlevison/high-performance-teams-game/issues/103\;

--- src/config/rounds/index.ts
+++ src/config/rounds/index.ts
@@ -5,5 +5,15 @@ import { round4 } from './round4';
 import { round5 } from './round5';
 import { round6 } from './round6';
 import { round7 } from './round7';
+import { round8 } from './round8';
 
-export const rounds = [round1, round2, round3, round4, round5, round6, round7];
+export const rounds = [
+  round1,
+  round2,
+  round3,
+  round4,
+  round5,
+  round6,
+  round7,
+  round8,
+];
---
+++ src/config/rounds/round8.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { RoundDescription } from '../../state';
+
+export const round8: RoundDescription = {
+  title: 'TODO: ADD TITLE',
+  description: <p>{/* TODO: ADD DESCRIPTION */}</p>,
+  actions: {},
+};
